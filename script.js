9de09b3a9dfe06ab6f53ce3b12f287a505810002
Carlos Quesada <caquesada@indracompany.com>
2022-03-28 21:48:10 -0500

    Modificación sección proyectos
    Se realiza una mejora en la animación cuando despliega los proyectos, lo hace ver mejor visualmente. Se realiza limpieza de codigo y se estandariza la parte de los titulos de las secciones, modificación del html, css y js

--- script.js
+++ script.js
@@ -140,28 +140,45 @@ projects.forEach((project, i) => {
 });
 
 // Projects Button
+const section3 = document.querySelector(".section-3");
 const projectsBtn = document.querySelector(".projects-btn");
 const projectsBtnText = document.querySelector(".projects-btn span");
 let showHideBool = true;
+
+const showProjects = (project, i) => {
+  setTimeout(() => {
+    project.style.display = "flex";
+    section3.scrollIntoView({ block: "end" });
+  }, 600);
+
+  setTimeout(() => {
+    project.style.opacity = "1";
+  }, i * 200);
+};
+
+const hideProjects = (project, i) => {
+  setTimeout(() => {
+    project.style.display = "none";
+    section3.scrollIntoView({ block: "end" });
+  }, 1200);
+
+  setTimeout(() => {
+    project.style.opacity = "0";
+  }, i * 100);
+};
 
 projectsBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change");
 
-  projects.forEach((project, i) => {
-    if (i >= 6) {
-      if (showHideBool) {
-        project.style.display = "flex";
-        project.style.opacity = "1";
+  showHideBool
+    ? (projectsBtnText.textContent = "Show Less")
+    : (projectsBtnText.textContent = "Show More");
 
-        projectsBtnText.textContent = "Show Less";
-      } else {
-        project.style.display = "none";
-        project.style.opacity = "0";
-        projectsBtnText.textContent = "Show More";
-      }
-    }
+  projects.forEach((project, i) => {
+    i >= 6 &&
+      (showHideBool ? showProjects(project, i) : hideProjects(project, i));
   });
 
   showHideBool = !showHideBool;
