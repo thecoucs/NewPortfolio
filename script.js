44244e9a0f4e7048afdaad01f96ed79860f65fea
Carlos Quesada <caquesada@indracompany.com>
2022-04-12 18:54:55 -0500

    Animacion para el puntero
    Se añade una clase en el html en los input del formulario, los proyectos y los items del menu, en el js se hace la parte lógica, se realiza el añadido de la los estilos a este elemento para que tome todo el elemento por el cual esta pasando encima el cursor

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
+let mouseCircleBool = true;
+
 const mouseCircleFn = (x, y) => {
-  mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
+  mouseCircleBool &&
+    (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`);
+
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px;opacity:1`;
 };
 //End of Mouse Circle
@@ -73,6 +77,35 @@ const stickyElement = (x, y, hoveredEl) => {
   //End of Sticky Elements
 };
 
+// Mouse Circle Transform
+const mouseCircleTransform = (hoveredEl) => {
+  if (hoveredEl.classList.contains("pointer-enter")) {
+    hoveredEl.onmousemove = () => {
+      mouseCircleBool = false;
+      mouseCircle.style.cssText = `width: ${
+        hoveredEl.getBoundingClientRect().width
+      }px; height: ${hoveredEl.getBoundingClientRect().height}px; top: ${
+        hoveredEl.getBoundingClientRect().top
+      }px;  left: ${
+        hoveredEl.getBoundingClientRect().left
+      }px; opacity: 1; transform: translate(0,0); animation: none; border-radius: ${
+        getComputedStyle(hoveredEl).borderBottomLeftRadius
+      }; transition: width .5s, height .5s, top .5s, left .5s, transform .5s,border-radius .5s`;
+    };
+
+    hoveredEl.onmouseleave = () => {
+      mouseCircleBool = true;
+    };
+
+    document.onscroll = () => {
+      if (!mouseCircleBool) {
+        mouseCircle.style.top = `${hoveredEl.getBoundingClientRect().top}px`;
+      }
+    };
+  }
+};
+// End Mouse Circle Transform
+
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
@@ -82,6 +115,8 @@ document.body.addEventListener("mousemove", (e) => {
 
   const hoveredEl = document.elementFromPoint(x, y);
   stickyElement(x, y, hoveredEl);
+
+  mouseCircleTransform(hoveredEl);
 });
 
 document.body.addEventListener("mouseleave", () => {
@@ -256,6 +291,8 @@ projects.forEach((project, i) => {
     document.body.style.overflowY = "hidden";
 
     document.removeEventListener("scroll", scrollFn);
+
+    mouseCircle.style.opacity = 0;
 
     progressBarFn(bigImgWrapper);
 
